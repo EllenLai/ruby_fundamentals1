1a2a68a3883962c27eac0721891cae3ca5eec040
EllenLai <miss.ellenlai@gmail.com>
2014-01-14 13:16:20 -0500

    Second Commit

---
+++ exercise2.rb
@@ -0,0 +1,24 @@
+# 1. calculating a good tip!
+
+puts 55 * 0.15
+
+# end calculating a good tip
+
+# 2. adding a string and an integer
+
+puts "Hello" + 26.to_s
+
+# 3. outputting the results from 45628 x 7839
+
+a = 45628
+b = 7839
+
+puts "a times b equals #{a*b}"
+
+# end outputting results
+
+# Figuring out the value of the expression, which is true!
+
+puts (true && false) || (false && true) || !(false && false)
+
+# end value of the expression
