03e7cc2a1ae6aac6a8056c08bf3ada4988d0b55f
EllenLai <miss.ellenlai@gmail.com>
2014-01-14 13:53:24 -0500

    Asking user for age and calculating their year of birth

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,13 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+puts "How old are you?"
+age = gets.chomp
+puts "Did your birthday past yet this year?"
+birthday = gets.chomp
+if
+	birthday == "yes"
+	puts "You were born in #{2014 - age.to_i}"
+else	
+	puts "You were born in #{2014 - age.to_i - 1}"
+end
